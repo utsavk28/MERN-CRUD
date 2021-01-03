56a927b6477d8e144be824368165c1547d5c19c5
utsavk28 <utsavkhatu416@gmail.com>
2021-01-03 18:21:08 +0530

    v1.1

--- config/db.js
+++ config/db.js
@@ -6,15 +6,11 @@ const uri = process.env.mongoURI;
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(uri, {
+        await mongoose.connect(db, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
         });
-        const connection = mongoose.connection;
-        connection.once('open', () => {
-            console.log("MongoDB database connection established successfully");
-        })
     } catch (error) {
         console.error(error.message);
         process.exit(1);
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require("path")
+
 const app = express();
 
 // Connect Database 
@@ -9,12 +11,22 @@ connectDB()
 app.use(express.json({ extended: false }));
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('API Running on port' + PORT);
-});
+// app.get('/', (req, res) => {
+//     res.send('API Running on port' + PORT);
+// });
 
 // Define Routes
-app.use("/api/posts",require("./routes/api/posts"))
+app.use("/api/posts", require("./routes/api/posts"))
+
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set Static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, (req, res) => {
     console.log(`Server started on port ${PORT}`);
